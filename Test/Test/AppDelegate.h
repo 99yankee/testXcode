0ebdd6b6ecfead5fb81ffa7d39aa619d8556d1c3
Steven <slyktey@gmail.com>
2015-12-01 14:17:31 -0500

    Test1

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Steven Lyktey on 12/1/15.
+//  Copyright © 2015 Steven Lyktey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
